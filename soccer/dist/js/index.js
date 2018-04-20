bf1299ea87a153669ce82ab8817f8fd45bc0c3d3
Fernando <fernand-o.a@hotmail.com>
2018-04-20 04:38:57 +0200

    Mejorada interfaz, creado sistema de torneos para potencias de 2

--- soccer/dist/js/index.js
+++ soccer/dist/js/index.js
@@ -2,46 +2,55 @@
 
 var elems = {
   boton: document.querySelector(".boton"),
-  partido: document.querySelector(".partido")
+  partido: document.querySelector(".partido"),
+  clasificatoria: document.querySelector(".clasificatoria")
 };
+
+var equiposRestantes = void 0;
+var posiblesResultados = void 0;
+var ronda = 1; //Ronda actual del "torneo". Empieza a contar en 1.
 
 elems.boton.onclick = nuevoPartido;
 
-var equipos = ["Alaves", "Athletic Bilbao", "Athletico Madrid", "Barcelona", "Celta de Vigo", "Deportivo La Coruña", "Eibar", "Espanyol", "Getafe", "Girona"];
-var equiposRestantes = void 0;
-var posiblesResultados = void 0;
+var partidos = []; /*
+                   {equipoA: "", equipoB: "", resultado = []}, //Resultado ["1", "5"] (Goles equipoA, Goles equipoB).
+                   {equipoA: "", equipoB: "", resultado = []},
+                   {equipoA: "", equipoB: "", resultado = []},
+                   {equipoA: "", equipoB: "", resultado = []},
+                   {equipoA: "", equipoB: "", resultado = []}
+                   ]; */
 
-var equipoA = {
-  nombre: "",
-  goles: ""
-};
+var equipos = [{ nombre: "Alaves", goles: 0, derrotado: false }, { nombre: "Athletic Bilbao", goles: 0, derrotado: false }, { nombre: "Athletico Madrid", goles: 0, derrotado: false }, { nombre: "Barcelona", goles: 0, derrotado: false }, { nombre: "Celta de Vigo", goles: 0, derrotado: false }, { nombre: "Deportivo La Coruña", goles: 0, derrotado: false }, { nombre: "Eibar", goles: 0, derrotado: false }, { nombre: "Espanyol", goles: 0, derrotado: false }];
 
-var equipoB = {
-  nombre: "",
-  goles: ""
-};
-
-var resultado = void 0;
-
-function generarProbabilidades() {
-  var ceros = _.fill(Array(7), 0);
-  var unos = _.fill(Array(6), 1);
-  var dos = _.fill(Array(5), 2);
-  var tres = _.fill(Array(4), 3);
-  posiblesResultados = _.concat(ceros, unos, dos, tres);
-  posiblesResultados = _.concat(posiblesResultados, [4, 5, 6, 7, 8, 9]);
-  posiblesResultados = _.shuffle(posiblesResultados);
+function decidirPartidos() {
+  //Empareja los equipos en cada ronda
+  for (var i in _.range(0, equiposRestantes.length / 2)) {
+    partidos.push({ equipoA: elegirEquipo(), equipoB: elegirEquipo(), resultado: [] });
+    partidos[i].resultado = decidirGanador(partidos[i].equipoA, partidos[i].equipoB);
+  }
 }
 
 function elegirEquipo() {
+  /*let equipo;
+  do{
+    equipo = equiposRestantes.shift();       //En las rondas distintas de la primera, se omitirán los equipos derrotados en rondas anteriores.
+  }while(equipo.derrotado == true); */
   var equipo = equiposRestantes.shift();
   return equipo;
 }
 
-function decidirGanador() {
-  //let ganador = Math.round(Math.random());  //0 o 1
-  //decidirGoles(ganador);
-  decidirGoles();
+function decidirGanador(equipoA, equipoB) {
+  var ind = Math.floor(Math.random() * posiblesResultados.length);
+  var ind2 = Math.floor(Math.random() * posiblesResultados.length);
+
+  equipoA.goles = posiblesResultados[ind];
+  equipoB.goles = posiblesResultados[ind2];
+  if (equipoA.goles > equipoB.goles || equipoA.goles == equipoB.goles) {
+    equipoB.derrotado = true;
+  } else {
+    equipoA.derrotado = true;
+  }
+  return [equipoA.goles, equipoB.goles];
 }
 
 /*function decidirGoles(ganador){
@@ -55,19 +64,76 @@ function decidirGanador() {
   }
 } */
 
-function decidirGoles(ganador) {
-  var ind = Math.floor(Math.random() * posiblesResultados.length);
-  var ind2 = Math.floor(Math.random() * posiblesResultados.length);
+function setResultados(ronda) {
+  var nexov = document.createElement("div");
+  nexov.classList.add("nexoV");
+  nexov.classList.add("v" + ronda);
 
-  equipoA.goles = posiblesResultados[ind];
-  equipoB.goles = posiblesResultados[ind2];
+  var nexoh = document.createElement("div");
+  nexoh.classList.add("nexoH");
+  nexoh.classList.add("h" + ronda);
+
+  var fila = document.createElement("div");
+  fila.classList.add("fila");
+  fila.classList.add("f" + ronda);
+
+  elems.clasificatoria.appendChild(nexov);
+  elems.clasificatoria.appendChild(nexoh);
+  elems.clasificatoria.appendChild(fila);
+
+  elems["v" + ronda] = document.querySelector(".v" + ronda);
+  elems["h" + ronda] = document.querySelector(".h" + ronda);
+  elems["f" + ronda] = document.querySelector(".f" + ronda);
+
+  for (var i in [0, 1, 2, 3]) {
+    var nexV = document.createElement("div");
+    var nexH = document.createElement("div");
+
+    nexV.classList.add("nexo");
+    nexH.classList.add("nexo");
+
+    nexV.classList.add("V");
+    nexH.classList.add("H" + ronda);
+
+    elems["v" + ronda].appendChild(nexV);
+    elems["h" + ronda].appendChild(nexH);
+  }
+
+  partidos.forEach(function (e, i) {
+    var teamA = document.createElement("div");
+    var teamB = document.createElement("div");
+
+    teamA.classList.add("equipo");
+    teamB.classList.add("equipo");
+
+    teamA.textContent = e.equipoA.nombre;
+    teamB.textContent = e.equipoB.nombre;
+
+    elems["f" + ronda].appendChild(teamA);
+    elems["f" + ronda].appendChild(teamB);
+  });
 }
 
 function nuevoPartido() {
+  partidos = [];
   equiposRestantes = _.shuffle(equipos);
-  equipoA.nombre = elegirEquipo();
-  equipoB.nombre = elegirEquipo();
-  generarProbabilidades();
-  decidirGanador();
-  elems.partido.textContent = equipoA.nombre + " " + equipoA.goles + " - " + equipoB.nombre + "-" + equipoB.goles;
+  equiposRestantes = equiposRestantes.filter(function (e) {
+    //Solo guarda los equipos que no estén derrotados.
+    if (e.derrotado === false) return true;
+  });
+  decidirPartidos(); //En cada ronda, empareja los equipos y decide el resultado del enfrentamiento.
+  setResultados(ronda);
+  ronda++;
 }
+
+function generarProbabilidades() {
+  var ceros = _.fill(Array(20), 0);
+  var unos = _.fill(Array(18), 1);
+  var dos = _.fill(Array(17), 2);
+  var tres = _.fill(Array(15), 3);
+  posiblesResultados = _.concat(ceros, unos, dos, tres);
+  posiblesResultados = _.concat(posiblesResultados, [4, 5, 6, 7, 8, 9]);
+  posiblesResultados = _.shuffle(posiblesResultados);
+}
+
+generarProbabilidades(); //Solo es necesario una vez (al iniciarse el programa).
--- soccer/src/js/index.es6
+++ soccer/src/js/index.es6
@@ -1,11 +1,16 @@
 let elems = {
   boton: document.querySelector(".boton"),
-  partido: document.querySelector(".partido")
+  partido: document.querySelector(".partido"),
+  clasificatoria: document.querySelector(".clasificatoria")
 }
+
+let equiposRestantes;
+let posiblesResultados;
+let ronda = 1;  //Ronda actual del "torneo". Empieza a contar en 1.
 
 elems.boton.onclick = nuevoPartido;
 
-/*let partidos =[
+let partidos =[]; /*
   {equipoA: "", equipoB: "", resultado = []}, //Resultado ["1", "5"] (Goles equipoA, Goles equipoB).
   {equipoA: "", equipoB: "", resultado = []},
   {equipoA: "", equipoB: "", resultado = []},
@@ -14,30 +19,31 @@ elems.boton.onclick = nuevoPartido;
 ]; */
 
 let equipos = [
-  {nombre: "Alaves", goles = ""},
-  {nombre: "Athletic Bilbao", goles = ""},
-  {nombre: "Athletico Madrid", goles = ""},
-  {nombre: "Barcelona", goles = ""},
-  {nombre: "Celta de Vigo", goles = ""},
-  {nombre: "Deportivo La Coruña", goles = ""},
-  {nombre: "Eibar", goles = ""},
-  {nombre: "Espanyol", goles = ""},
-  {nombre: "Getafe", goles = ""},
-  {nombre: "Girona", goles = ""}
+  {nombre: "Alaves", goles: 0, derrotado: false},
+  {nombre: "Athletic Bilbao", goles: 0, derrotado: false},
+  {nombre: "Athletico Madrid", goles: 0, derrotado: false},
+  {nombre: "Barcelona", goles: 0, derrotado: false},
+  {nombre: "Celta de Vigo", goles: 0, derrotado: false},
+  {nombre: "Deportivo La Coruña", goles: 0, derrotado: false},
+  {nombre: "Eibar", goles: 0, derrotado: false},
+  {nombre: "Espanyol", goles: 0, derrotado: false},
 ];
 
-let equiposRestantes;
-let posiblesResultados;
 
-function decidirPartidos(){
 
-  for(let i in _.range(0, (equipos.length/2)+1)){
-    partidos.push({equipoA: elegirEquipo(), equipoB: elegirEquipo(), resultado = decidirGanador(equipoA, equipoB)});
+function decidirPartidos(){                      //Empareja los equipos en cada ronda
+  for(let i in _.range(0, (equiposRestantes.length/2))){
+    partidos.push({equipoA: elegirEquipo(), equipoB: elegirEquipo(), resultado: []});
+    partidos[i].resultado = decidirGanador(partidos[i].equipoA, partidos[i].equipoB);
   }
 }
 
 function elegirEquipo(){
-   let equipo = equiposRestantes.shift();
+  /*let equipo;
+  do{
+    equipo = equiposRestantes.shift();       //En las rondas distintas de la primera, se omitirán los equipos derrotados en rondas anteriores.
+  }while(equipo.derrotado == true); */
+  let equipo = equiposRestantes.shift();
    return equipo;
 }
 
@@ -47,7 +53,11 @@ function decidirGanador(equipoA, equipoB){
 
   equipoA.goles = posiblesResultados[ind];
   equipoB.goles = posiblesResultados[ind2];
-
+  if(equipoA.goles > equipoB.goles || equipoA.goles == equipoB.goles){
+    equipoB.derrotado = true;
+  }else{
+    equipoA.derrotado = true;
+  }
   return [equipoA.goles, equipoB.goles];
 }
 
@@ -62,22 +72,79 @@ function decidirGanador(equipoA, equipoB){
   }
 } */
 
+function setResultados(ronda){
+  let nexov = document.createElement("div");
+  nexov.classList.add("nexoV");
+  nexov.classList.add(`v${ronda}`);
+
+  let nexoh = document.createElement("div");
+  nexoh.classList.add("nexoH");
+  nexoh.classList.add(`h${ronda}`);
+
+  let fila = document.createElement("div");
+  fila.classList.add("fila");
+  fila.classList.add(`f${ronda}`);
+
+  elems.clasificatoria.appendChild(nexov);
+  elems.clasificatoria.appendChild(nexoh);
+  elems.clasificatoria.appendChild(fila);
+
+  elems[`v${ronda}`] = document.querySelector(`.v${ronda}`);
+  elems[`h${ronda}`] = document.querySelector(`.h${ronda}`);
+  elems[`f${ronda}`] = document.querySelector(`.f${ronda}`);
+
+  for(let i in [0, 1, 2, 3]){
+    let nexV = document.createElement("div");
+    let nexH = document.createElement("div");
+
+    nexV.classList.add("nexo");
+    nexH.classList.add("nexo");
+
+    nexV.classList.add("V");
+    nexH.classList.add(`H${ronda}`);
+
+    elems[`v${ronda}`].appendChild(nexV);
+    elems[`h${ronda}`].appendChild(nexH);
+  }
+
+  partidos.forEach((e, i) =>{
+    let teamA = document.createElement("div");
+    let teamB = document.createElement("div");
+
+    teamA.classList.add("equipo");
+    teamB.classList.add("equipo");
+
+    teamA.textContent = e.equipoA.nombre;
+    teamB.textContent = e.equipoB.nombre;
+
+    elems[`f${ronda}`].appendChild(teamA);
+    elems[`f${ronda}`].appendChild(teamB);
+  });
+}
+
+
 function nuevoPartido(){
+  partidos = [];
   equiposRestantes = _.shuffle(equipos);
-  decidirPartidos();
-  generarProbabilidades()
-  decidirGanador();
-  elems.partido.textContent = `${equipoA.nombre} ${equipoA.goles} - ${equipoB.nombre}-${equipoB.goles}`
+  equiposRestantes = equiposRestantes.filter(e=>{   //Solo guarda los equipos que no estén derrotados.
+    if(e.derrotado===false)
+      return true;
+  });
+  decidirPartidos();  //En cada ronda, empareja los equipos y decide el resultado del enfrentamiento.
+  setResultados(ronda);
+  ronda++;
 }
 
 
 
 function generarProbabilidades(){
-  let ceros = _.fill(Array(7), 0);
-  let unos = _.fill(Array(6), 1);
-  let dos = _.fill(Array(5), 2);
-  let tres = _.fill(Array(4), 3);
+  let ceros = _.fill(Array(20), 0);
+  let unos = _.fill(Array(18), 1);
+  let dos = _.fill(Array(17), 2);
+  let tres = _.fill(Array(15), 3);
   posiblesResultados = _.concat(ceros, unos, dos, tres);
   posiblesResultados = _.concat(posiblesResultados, [4, 5, 6, 7, 8, 9]);
   posiblesResultados = _.shuffle(posiblesResultados);
 }
+
+generarProbabilidades();  //Solo es necesario una vez (al iniciarse el programa).
